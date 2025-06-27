addef4dbcba4f05dae87de996407c027ab7d3f6c
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-06-27 14:13:41 +0000

    fix: remove local from rewrites

--- rewrites.js
+++ rewrites.js
@@ -5,27 +5,25 @@ const rewrites = async () => {
 			destination: 'https://plausible.io/js/script.js',
 		},
 		{
-			source: '/plausible/js/plausible.hash.local.outbound-links.js',
+			source: '/plausible/js/plausible.hash.outbound-links.js',
+			destination: 'https://plausible.io/js/plausible.hash.outbound-links.js',
+		},
+		{
+			source: '/plausible/js/plausible.file-downloads.hash.outbound-links.js',
 			destination:
-				'https://plausible.io/js/plausible.hash.local.outbound-links.js',
+				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.js',
 		},
 		{
 			source:
-				'/plausible/js/plausible.file-downloads.hash.local.outbound-links.js',
+				'/plausible/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
 			destination:
-				'https://plausible.io/js/plausible.file-downloads.hash.local.outbound-links.js',
+				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
 		},
 		{
 			source:
-				'/plausible/js/plausible.file-downloads.hash.local.outbound-links.tagged-events.js',
+				'/plausible/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
 			destination:
-				'https://plausible.io/js/plausible.file-downloads.hash.local.outbound-links.tagged-events.js',
-		},
-		{
-			source:
-				'/plausible/js/script.file-downloads.hash.local.outbound-links.pageview-props.tagged-events.js',
-			destination:
-				'https://plausible.io/js/script.file-downloads.hash.local.outbound-links.pageview-props.tagged-events.js',
+				'https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
 		},
 		{
 			source: '/api/event',
