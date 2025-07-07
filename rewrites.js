57ff00545a4ed51915c33af98e73cc4ccf3a22ed
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-07-07 16:18:23 +0000

    fix: upsie

--- rewrites.js
+++ rewrites.js
@@ -1,8 +1,29 @@
 const rewrites = async () => {
 	return [
 		{
-			source: '/plausible/:path*',
-			destination: 'https://plausible.io/:path*',
+			source: '/plausible/js/script.js',
+			destination: 'https://plausible.io/js/script.js',
+		},
+		{
+			source: '/plausible/js/plausible.hash.outbound-links.js',
+			destination: 'https://plausible.io/js/plausible.hash.outbound-links.js',
+		},
+		{
+			source: '/plausible/js/plausible.file-downloads.hash.outbound-links.js',
+			destination:
+				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.js',
+		},
+		{
+			source:
+				'/plausible/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
+			destination:
+				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
+		},
+		{
+			source:
+				'/plausible/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
+			destination:
+				'https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
 		},
 		{
 			source: '/api/event',
