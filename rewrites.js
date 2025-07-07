d99c0c48965fa4d39f3f1fe298e842de8e12e699
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-07-07 16:09:26 +0000

    chore: simplify rewrites and add vercel speed insights

--- rewrites.js
+++ rewrites.js
@@ -1,29 +1,8 @@
 const rewrites = async () => {
 	return [
 		{
-			source: '/plausible/js/script.js',
-			destination: 'https://plausible.io/js/script.js',
-		},
-		{
-			source: '/plausible/js/plausible.hash.outbound-links.js',
-			destination: 'https://plausible.io/js/plausible.hash.outbound-links.js',
-		},
-		{
-			source: '/plausible/js/plausible.file-downloads.hash.outbound-links.js',
-			destination:
-				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.js',
-		},
-		{
-			source:
-				'/plausible/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
-			destination:
-				'https://plausible.io/js/plausible.file-downloads.hash.outbound-links.tagged-events.js',
-		},
-		{
-			source:
-				'/plausible/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
-			destination:
-				'https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js',
+			source: '/plausible/:path*',
+			destination: 'https://plausible.io/:path*',
 		},
 		{
 			source: '/api/event',
--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { ghost, greed, tagada } from '@/modules/utilities/customFonts';
 import '@/app/(frontend)/[locale]/global.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
@@ -95,6 +96,7 @@ export default async function RootLayout({ children, params }: Readonly<Args>) {
 
 	return (
 		<html lang={locale} className='scroll-smooth'>
+			<SpeedInsights />
 			<Plausible>
 				<NextIntlClientProvider messages={messages}>
 					<ReactLenis root>
